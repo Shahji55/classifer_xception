ef4dea93a4867ed424437d04e50a3faee2ce2430
Syed K <shahzeb_kirmani@hotmail.com>
2021-06-09 11:38:44 +0500

    Training/validation of classifier and saving best model

---
+++ train_classifier_final.py
@@ -0,0 +1,334 @@
+"""
+Creates an Xception Model as defined in:
+Francois Chollet
+Xception: Deep Learning with Depthwise Separable Convolutions
+https://arxiv.org/pdf/1610.02357.pdf
+This weights ported from the Keras implementation. Achieves the following performance on the validation set:
+Loss:0.9173 Prec@1:78.892 Prec@5:94.292
+REMEMBER to set your image size to 3x299x299 for both test and validation
+normalize = transforms.Normalize(mean=[0.5, 0.5, 0.5],
+                                  std=[0.5, 0.5, 0.5])
+The resize parameter of the validation transform should be 333, and make sure to center crop at 299x299
+"""
+import math
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.utils.model_zoo as model_zoo
+from torch.nn import init
+import torch
+import matplotlib.pyplot as plt
+import torch
+from torchvision import datasets, transforms
+from torch.utils.data import Dataset, DataLoader
+import os
+import numpy as np
+import torchvision
+import torch.optim as optim
+
+__all__ = ['xception']
+
+model_urls = {
+    'xception': 'https://www.dropbox.com/s/1hplpzet9d7dv29/xception-c0a72b38.pth.tar?dl=1'
+}
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+print("Using {} device".format(device))
+
+
+class SeparableConv2d(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size=1, stride=1, padding=0, dilation=1, bias=False):
+        super(SeparableConv2d, self).__init__()
+
+        self.conv1 = nn.Conv2d(in_channels, in_channels, kernel_size, stride, padding, dilation, groups=in_channels,
+                               bias=bias)
+        self.pointwise = nn.Conv2d(in_channels, out_channels, 1, 1, 0, 1, 1, bias=bias)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.pointwise(x)
+        return x
+
+
+class Block(nn.Module):
+    def __init__(self, in_filters, out_filters, reps, strides=1, start_with_relu=True, grow_first=True):
+        super(Block, self).__init__()
+
+        if out_filters != in_filters or strides != 1:
+            self.skip = nn.Conv2d(in_filters, out_filters, 1, stride=strides, bias=False)
+            self.skipbn = nn.BatchNorm2d(out_filters)
+        else:
+            self.skip = None
+
+        self.relu = nn.ReLU(inplace=True)
+        rep = []
+
+        filters = in_filters
+        if grow_first:
+            rep.append(self.relu)
+            rep.append(SeparableConv2d(in_filters, out_filters, 3, stride=1, padding=1, bias=False))
+            rep.append(nn.BatchNorm2d(out_filters))
+            filters = out_filters
+
+        for i in range(reps - 1):
+            rep.append(self.relu)
+            rep.append(SeparableConv2d(filters, filters, 3, stride=1, padding=1, bias=False))
+            rep.append(nn.BatchNorm2d(filters))
+
+        if not grow_first:
+            rep.append(self.relu)
+            rep.append(SeparableConv2d(in_filters, out_filters, 3, stride=1, padding=1, bias=False))
+            rep.append(nn.BatchNorm2d(out_filters))
+
+        if not start_with_relu:
+            rep = rep[1:]
+        else:
+            rep[0] = nn.ReLU(inplace=False)
+
+        if strides != 1:
+            rep.append(nn.MaxPool2d(3, strides, 1))
+        self.rep = nn.Sequential(*rep)
+
+    def forward(self, inp):
+        x = self.rep(inp)
+
+        if self.skip is not None:
+            skip = self.skip(inp)
+            skip = self.skipbn(skip)
+        else:
+            skip = inp
+
+        x += skip
+        return x
+
+
+class Xception(nn.Module):
+    """
+    Xception optimized for the ImageNet dataset, as specified in
+    https://arxiv.org/pdf/1610.02357.pdf
+    """
+
+    def __init__(self, num_classes=2):
+        """ Constructor
+        Args:
+            num_classes: number of classes
+        """
+        super(Xception, self).__init__()
+
+        self.num_classes = num_classes
+
+        self.conv1 = nn.Conv2d(3, 32, 3, 2, 0, bias=False)
+        self.bn1 = nn.BatchNorm2d(32)
+        self.relu = nn.ReLU(inplace=True)
+
+        self.conv2 = nn.Conv2d(32, 64, 3, bias=False)
+        self.bn2 = nn.BatchNorm2d(64)
+        # do relu here
+
+        self.block1 = Block(64, 128, 2, 2, start_with_relu=False, grow_first=True)
+        self.block2 = Block(128, 256, 2, 2, start_with_relu=True, grow_first=True)
+        self.block3 = Block(256, 728, 2, 2, start_with_relu=True, grow_first=True)
+
+        self.block4 = Block(728, 728, 3, 1, start_with_relu=True, grow_first=True)
+        self.block5 = Block(728, 728, 3, 1, start_with_relu=True, grow_first=True)
+        self.block6 = Block(728, 728, 3, 1, start_with_relu=True, grow_first=True)
+        self.block7 = Block(728, 728, 3, 1, start_with_relu=True, grow_first=True)
+
+        self.block8 = Block(728, 728, 3, 1, start_with_relu=True, grow_first=True)
+        self.block9 = Block(728, 728, 3, 1, start_with_relu=True, grow_first=True)
+        self.block10 = Block(728, 728, 3, 1, start_with_relu=True, grow_first=True)
+        self.block11 = Block(728, 728, 3, 1, start_with_relu=True, grow_first=True)
+
+        self.block12 = Block(728, 1024, 2, 2, start_with_relu=True, grow_first=False)
+
+        self.conv3 = SeparableConv2d(1024, 1536, 3, 1, 1)
+        self.bn3 = nn.BatchNorm2d(1536)
+
+        # do relu here
+        self.conv4 = SeparableConv2d(1536, 2048, 3, 1, 1)
+        self.bn4 = nn.BatchNorm2d(2048)
+
+        self.fc = nn.Linear(2048, num_classes)
+
+        # ------- init weights --------
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+                m.weight.data.normal_(0, math.sqrt(2. / n))
+            elif isinstance(m, nn.BatchNorm2d):
+                m.weight.data.fill_(1)
+                m.bias.data.zero_()
+        # -----------------------------
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+
+        x = self.conv2(x)
+        x = self.bn2(x)
+        x = self.relu(x)
+
+        x = self.block1(x)
+        x = self.block2(x)
+        x = self.block3(x)
+        x = self.block4(x)
+        x = self.block5(x)
+        x = self.block6(x)
+        x = self.block7(x)
+        x = self.block8(x)
+        x = self.block9(x)
+        x = self.block10(x)
+        x = self.block11(x)
+        x = self.block12(x)
+
+        x = self.conv3(x)
+        x = self.bn3(x)
+        x = self.relu(x)
+
+        x = self.conv4(x)
+        x = self.bn4(x)
+        x = self.relu(x)
+
+        x = F.adaptive_avg_pool2d(x, (1, 1))
+        x = x.view(x.size(0), -1)
+        x = self.fc(x)
+
+        return x
+
+
+def xception(pretrained=False, **kwargs):
+    """
+    Construct Xception.
+    """
+
+    model = Xception(**kwargs)
+    if pretrained:
+        model.load_state_dict(model_zoo.load_url(model_urls['xception']))
+    return model
+
+
+def imshow(img):
+    img = img / 2 + 0.5     # unnormalize
+    npimg = img.numpy()
+    plt.imshow(np.transpose(npimg, (1, 2, 0)))
+    plt.show()
+
+
+net = Xception()
+net = net.cuda()
+
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+
+cwd = os.getcwd()
+data_dir = cwd + '/data'
+
+print(data_dir)
+
+transform = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Resize((150, 50)),
+    # transforms.Normalize(mean=(0.0, 0.0, 0.0), std=(1.0, 1.0, 1.0))
+    # transforms.Normalize((0.0, 0.0, 0.0), (1.0, 1.0, 1.0))
+    # transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])
+])
+
+dataset = datasets.ImageFolder(data_dir, transform=transform)
+
+dataloader = torch.utils.data.DataLoader(dataset, batch_size=32, shuffle=True)
+
+images, labels = next(iter(dataloader))
+
+# Display image and label.
+
+print(f"Feature batch shape: {images.size()}")
+print(f"Labels batch shape: {labels.size()}")
+img = images[0].squeeze()
+# print(img)
+img = np.array(img)
+img = img.transpose(1, 2, 0)
+label = labels[0]
+plt.imshow(img, cmap="gray")
+# plt.show()
+# print(f"Label: {label}")
+
+train_set, test_set = torch.utils.data.random_split(dataset, [7658, 1914], generator=torch.Generator().manual_seed(0))
+print(len(train_set))
+print(len(test_set))
+# print(train_set.indices)
+# print(test_set.indices)
+
+
+batch_size = 32
+# Dataloader for training set
+train_loader = torch.utils.data.DataLoader(train_set, batch_size=batch_size, shuffle=True)
+
+# Dataloader for test set
+test_loader = torch.utils.data.DataLoader(test_set, batch_size=batch_size, shuffle=True)
+
+use_cuda = torch.cuda.is_available()
+
+min_valid_loss = np.inf
+
+for epoch in range(20):  # loop over the dataset multiple times
+    train_loss = 0.0
+    valid_loss = 0.0
+
+    print("------ Training ---------------")
+    for batch_idx, data in enumerate(train_loader, 0):
+
+        inputs, labels = data
+
+        # Transfer Data to GPU if available
+        if use_cuda:
+              inputs, labels = inputs.cuda(), labels.cuda()
+
+        # Clear the gradients
+        optimizer.zero_grad()
+
+        # Forward Pass
+        outputs = net(inputs)
+
+        # Find the Loss
+        loss = criterion(outputs, labels)
+
+        # Calculate gradients
+        loss.backward()
+
+        # Update Weights
+        optimizer.step()
+
+        # Calculate Loss
+        train_loss = train_loss + ((1 / (batch_idx + 1)) * (loss.data - train_loss))
+        print('Epoch: %d - Batch: %5d' % (epoch + 1, batch_idx + 1))
+
+    print("---------------------Validation------------------------------")
+    for batch_idx, data in enumerate(test_loader, 0):
+
+        # Transfer Data to GPU if available
+        if use_cuda:
+            inputs, labels = inputs.cuda(), labels.cuda()
+
+        # forward + backward + optimize
+        outputs = net(inputs)
+        loss = criterion(outputs, labels)
+
+        # update average validation loss
+        valid_loss = valid_loss + ((1 / (batch_idx + 1)) * (loss.data - valid_loss))
+        print('Epoch: %d - Batch: %5d' % (epoch + 1, batch_idx + 1))
+
+    # calculate average losses
+    train_loss = train_loss / len(train_set)
+    valid_loss = valid_loss / len(test_set)
+
+    # print training/validation statistics
+    print('Epoch: {} \tTraining Loss: {:.6f} \tValidation Loss: {:.6f}'.format(epoch + 1, train_loss, valid_loss))
+
+    if min_valid_loss > valid_loss:
+        print(f'Validation Loss Decreased({min_valid_loss:.6f}--->{valid_loss:.6f}) \t Saving The Model')
+        min_valid_loss = valid_loss
+        # Saving State Dict
+        torch.save(net.state_dict(), os.getcwd() + '/saved_models/' + str(epoch + 1) + "_" +
+                   str(valid_loss.item()) + "_" + 'saved_model.pth')
+
